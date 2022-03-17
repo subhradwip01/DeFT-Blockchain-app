a9b325724720219d803887c8674c64171ea82c1d
subhradwip01 <subhradwip01@gmail.com>
2022-03-17 16:38:50 +0530

    HerSlider bug fix
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/components/HeroSlider.jsx
+++ client/src/components/HeroSlider.jsx
@@ -3,12 +3,12 @@ import main from "../images/main.png"
 const HeroSlider = () => {
   return (
     <div className='flex w-full justify-center items-center flex-wrap'>
-        <div className='mx-auto my-auto'>
+        <div className='flex md:flex-1 flex-auto w-full flex-col justify-center items-center'>
             <h1 className='md:text-[12rem] text-[5rem] font-[1000]'>DeFt</h1>
             <p className='font-semibold'>A Decentralized finance system</p>
             <p className='font-semibold'>Send Ethereum accross the world easily</p>
         </div>
-        <div className='flex justify-center align-items-center sm:order-last'>
+        <div className='flex md:flex-1 flex-auto w-full flex-col justify-center items-center'>
             <img className='md:w-[500px] w-[300px] md:h-[500px] h-[300px]' src={main} alt="Logo"/>
         </div>
 
