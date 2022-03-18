de9df7840b142217b9570752386459a68a41aa63
subhradwip01 <subhradwip01@gmail.com>
2022-03-18 22:53:20 +0530

    Smart contract created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENCED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Transfar(
+        address from,
+        address reciever,
+        uint256 amount,
+        uint256 timestamp
+    );
+
+    struct TransfarStruct {
+        address from;
+        address reciever;
+        uint256 amount;
+        uint256 timestamp;
+    }
+
+    TransfarStruct[] transactions;
+
+    function addToBlockChain(address payable reciever,uint amount) public {
+        transactionCounter+=1;
+        transactions.push(TransfarStruct(msg.sender,reciever,amount,block.timestamp));
+        emit Transfar(msg.sender, reciever, amount, block.timestamp);
+    } 
+
+    function getAllTransactions() public view returns (TransfarStruct[] memory) {
+
+    }
+
+    function getTheTransactionCount() public view returns (uint256){
+
+    } 
+}
