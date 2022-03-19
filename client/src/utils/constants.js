f9a82c9380f92dafc2b4ef9cb33a07c926f685ee
subhradwip01 <subhradwip01@gmail.com>
2022-03-19 19:21:17 +0530

    Modified smart contract added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json"
 
 export const contractABI = abi.abi;
-export const contractAddress="0x87C99C0f437E059392eD89C0813d67A187F0c1c5"
+export const contractAddress="0x716721A8b3E872c7236a8a65af9e1087cB19CC27"
