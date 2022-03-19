231a71aa2e7dc8ee59f703fce0aac582c6d202a7
subhradwip01 <subhradwip01@gmail.com>
2022-03-19 19:29:00 +0530

    address shortner modified
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/utils/addressShortner.js
+++ client/src/utils/addressShortner.js
@@ -1,3 +1,3 @@
 export default function(address) {
-    return `${address.substr(0,5)}...${address.substr(address.length-4,address.length-1)}`
+    return `${address?.substr(0,5)}...${address.substr(address.length-4,address.length-1)}`
 }
